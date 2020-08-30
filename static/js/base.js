8dc0f80fe4aaca41c009ed4c7497083a3f26abf9
nico britz <nico.britz@outlook.com>
2020-08-30 14:23:12 +0100

    :art: added responsiveness for the navigation menu including media queries

--- static/js/base.js
+++ static/js/base.js
@@ -1,17 +1,16 @@
 $(document).ready(function () {
-    // Show mobile menu
+    // Show mobile menu on click
     $(".navbar-burger").click(function () {
         $(".navbar-burger").toggleClass("is-active");
         $("#navbar-menu").toggleClass("is-active");
     });
 
-    //Spin logo on hover
+    // Spin logo on mouse hover
     $(".main-logo-container").mouseenter(function () {
         if (!$('#logo-image').hasClass(".image-spin")) {
             $('#logo-image').addClass("image-spin")
         }
     }).mouseleave(function () {
-
             $('#logo-image').removeClass("image-spin");
         }
     )
