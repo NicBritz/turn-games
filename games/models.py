b1111df56ea72ad9df605c67129bda0c81bae1c0
nico britz <nico.britz@outlook.com>
2020-09-04 17:45:54 +0100

    :sparkles: added game model structure

---
+++ games/models.py
@@ -0,0 +1,64 @@
+from django.db import models
+
+
+# Category Model
+class Category(models.Model):
+    """ This model handles all the fields for a game category"""
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
+# Genre Model
+class Genre(models.Model):
+    """ This model handles all the fields for a game genre """
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
+# Tags Model
+class Tags(models.Model):
+    """ This model handles all the fields for a game tag """
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
+# Games Model
+class Game(models.Model):
+    """ This model handles all the fields for a game entry """
+    name = models.CharField(max_length=254)
+    description = models.TextField()
+    header_image_url = models.URLField(max_length=1024, null=True, blank=True)
+    header_image = models.ImageField(null=True, blank=True)
+    release_date = models.DateField(null=True, blank=True)
+    developer = models.CharField(max_length=254, null=True, blank=True)
+    publisher = models.CharField(max_length=254, null=True, blank=True)
+    platforms = models.CharField(max_length=254, null=True, blank=True)
+    required_age = models.DecimalField(max_digits=3, decimal_places=0, null=True, blank=True)
+    positive_ratings = models.DecimalField(max_digits=4, decimal_places=0, null=True, blank=True)
+    negative_ratings = models.DecimalField(max_digits=4, decimal_places=0, null=True, blank=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+
+    categories = models.ManyToManyField(Category)
+    genres = models.ManyToManyField(Genre)
+    tags = models.ManyToManyField(Tags)
+
+    def __str__(self):
+        return self.name
