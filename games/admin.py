43ab970c28c1eb7f702c387f0dc404bb78c8b150
nico britz <nico.britz@outlook.com>
2020-09-05 13:19:53 +0100

    :bug: fixed typo in admin.py that would crash the website

--- games/admin.py
+++ games/admin.py
@@ -5,31 +5,33 @@ from .models import Game, Category, Genre, Tag
 
 # clean up Game admin in django admin area
 class GameAdmin(admin.ModelAdmin):
-    list_display = (
-        'name',
-        'developer',
-        'publisher',
-        'price'
-    )
+    list_display = ("name", "description", "developer", "publisher", "price", "featured")
+    ordering = ("name",)
 
 
 # clean up Category admin in django admin area
 class CategoryAdmin(admin.ModelAdmin):
-    pass
+    list_display = ("friendly_name", "name")
+
+    ordering = ("name",)
 
 
 # clean up Genre admin in django admin area
 class GenreAdmin(admin.ModelAdmin):
-    pass
+    list_display = ("friendly_name", "name")
+
+    ordering = ("name",)
 
 
 # clean up Tag admin in django admin area
 class TagAdmin(admin.ModelAdmin):
-    pass
+    list_display = ("friendly_name", "name")
+
+    ordering = ("name",)
 
 
 # Register the application models
-admin.site.register(Game)
-admin.site.register(Category)
-admin.site.register(Genre)
-admin.site.register(Tag)
+admin.site.register(Game, GameAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Genre, GenreAdmin)
+admin.site.register(Tag, TagAdmin)
