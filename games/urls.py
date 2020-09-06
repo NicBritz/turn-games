0ea211513981f33a8d56f1ddd6267cb81417ccae
nico britz <nico.britz@outlook.com>
2020-09-06 17:00:02 +0100

    :sparkles: added a base details view for the games

--- games/urls.py
+++ games/urls.py
@@ -2,5 +2,6 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.all_games, name="games")
+    path('', views.all_games, name="games"),
+    path('<game_id>', views.game_detail, name="game_detail"),
 ]
--- games/views.py
+++ games/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render
-from .models import Game
+from django.shortcuts import render, get_object_or_404
+from .models import Game, Category
 
 
 def all_games(request):
-    """ returns the all products view - it also handles teh sorting ands searching functionality """
+    """ returns the all products view  """
 
     games = Game.objects.all()
 
@@ -11,3 +11,14 @@ def all_games(request):
         'games': games,
     }
     return render(request, 'games/games.html', context)
+
+
+def game_detail(request, game_id):
+    """ returns the game detail view with all the games information """
+
+    game = get_object_or_404(Game, pk=game_id)
+
+    context = {
+        'game': game,
+    }
+    return render(request, 'games/game_detail.html', context)
