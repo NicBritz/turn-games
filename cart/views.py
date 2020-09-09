6349d31007acb4cb1ec7b4a61506d876fa20a815
nico britz <nico.britz@outlook.com>
2020-09-09 15:49:09 +0100

    :sparkles: added the ability to remove an item from the bag

--- cart/views.py
+++ cart/views.py
@@ -9,7 +9,7 @@ def view_cart(request):
 def add_to_cart(request, game_id):
     """ Adds a game to the cart """
     redirect_url = request.POST.get('redirect_url')
-    cart = request.session.get('cart',[])
+    cart = request.session.get('cart', [])
 
     if game_id not in cart:
         cart.append(game_id)
@@ -17,3 +17,16 @@ def add_to_cart(request, game_id):
     request.session['cart'] = cart
 
     return redirect(redirect_url)
+
+
+def remove_from_cart(request, game_id):
+    """ Removes a game from the cart """
+
+    cart = request.session.get('cart', [])
+
+    if str(game_id) in cart:
+        cart.remove(game_id)
+
+    request.session['cart'] = cart
+
+    return redirect('/cart/')
