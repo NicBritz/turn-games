20bdadf54aef38e42b4d1dacd877624d3bb9cd93
nico britz <nico.britz@outlook.com>
2020-09-09 18:33:52 +0100

    :hammer: added messages to more view functions and refactored some code

--- cart/contexts.py
+++ cart/contexts.py
@@ -7,23 +7,28 @@ from games.models import Game
 
 def cart_contents(request):
     """ bag contents context processor to keep track of bag items """
-    cart = request.session.get('cart', [])
+    cart = request.session.get("cart", [])
     cart_items = []
     total = 0
 
+    # iterate through the items in the cart
     for item in cart:
+        # get the database record
         game = get_object_or_404(Game, pk=item)
+        # calculate the discount
         discount = game.price * (game.discount_percent / Decimal(100))
+        # calculate the discounted price
         discounted_price = game.price - discount
+        # add the price to the total
         total += discounted_price
 
-        cart_items.append({
-            'game_id': item,
-            'game_discount_price': discounted_price,
-            'game': game,
-        })
-
+        # append the values to the cart items list
+        cart_items.append(
+            {"game_id": item, "game_discount_price": discounted_price, "game": game,}
+        )
+    # calculate the tax
     tax = total * Decimal(settings.TAX_PERCENTAGE)
+    # calculate the grand total
     grand_total = total + tax
 
     context = {
--- cart/views.py
+++ cart/views.py
@@ -1,36 +1,62 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, HttpResponse, get_object_or_404
 from django.contrib import messages
 
 from games.models import Game
 
+
 def view_cart(request):
-    """ view the items in the shopping cart """
-    return render(request, 'cart/cart.html')
+    """ renders a view to see the games in the shopping cart """
+    return render(request, "cart/cart.html")
 
 
 def add_to_cart(request, game_id):
-    """ Adds a game to the cart """
-    game = Game.objects.get(pk=game_id)
-    redirect_url = request.POST.get('redirect_url')
-    cart = request.session.get('cart', [])
+    """ Adds a game to the shopping cart using the game_id passed from the games detail page """
+    try:
+        game = get_object_or_404(Game, pk=game_id)
 
-    if game_id not in cart:
-        cart.append(game_id)
+        # a redirect url to do back to the previous page
+        redirect_url = request.POST.get("redirect_url")
+        # get the cart list from the session or create one
+        cart = request.session.get("cart", [])
 
-    request.session['cart'] = cart
-    messages.info(request, f'Added {game.name} to your cart')
+        # only add a game to the cart if it is not in the cart already
+        if game_id not in cart:
+            cart.append(game_id)
+            # Success message
+            messages.success(request, f"Added {game.name} to your cart!")
+        else:
+            # Error message
+            messages.error(request, f"{game.name} is already in your cart!")
+
+        # update the session list
+        request.session["cart"] = cart
+
+    except Exception as e:
+        messages.error(request, f"Error adding item: {e}")
+        return HttpResponse(status=500)
 
     return redirect(redirect_url)
 
 
 def remove_from_cart(request, game_id):
-    """ Removes a game from the cart """
+    """ Removes a game from the cart using the game_id """
+    try:
 
-    cart = request.session.get('cart', [])
+        game = get_object_or_404(Game, pk=game_id)
 
-    if str(game_id) in cart:
-        cart.remove(game_id)
+        cart = request.session.get("cart", [])
+        # only delete a game if it is in the cart already
+        if str(game_id) in cart:
+            cart.remove(game_id)
+            # Warning message
+            messages.warning(request, f"Removed {game.name} from your cart!")
+        else:
+            # Error message
+            messages.error(request, f"Game not found in your cart!")
+        # update the session list
+        request.session["cart"] = cart
+    except Exception as e:
+        messages.error(request, f"Error removing item: {e}")
+        return HttpResponse(status=500)
 
-    request.session['cart'] = cart
-
-    return redirect('/cart/')
+    return redirect("/cart/")
