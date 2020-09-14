469818e5455f0c79918842353a9bcdc1c6b10f61
nico britz <nico.britz@outlook.com>
2020-09-14 13:29:59 +0100

    :sparkles: added basic profile view

---
+++ profiles/forms.py
@@ -0,0 +1,25 @@
+from django import forms
+from .models import UserProfile
+
+
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        exclude = ("user",)
+
+    def __init__(self, *args, **kwargs):
+        """
+        Add initial settings
+        """
+        super().__init__(*args, **kwargs)
+
+        # Make the fullname autofocused
+        self.fields["default_phone_number"].widget.attrs["autofocus"] = True
+
+        for field in self.fields:
+            if field == "default_country":
+                # country field has select class
+                self.fields[field].widget.attrs["class"] = " "
+            else:
+                # all other fields have input class
+                self.fields[field].widget.attrs["class"] = "input"
--- profiles/views.py
+++ profiles/views.py
@@ -1,10 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from .models import UserProfile
+from .forms import UserProfileForm
+from django.contrib import messages
 
 
+@login_required
 def profile(request):
-    """ Display the user's profiles. """
+    """
+    Display the user's profile
+    """
+    # get the users profile
+    profile = get_object_or_404(UserProfile, user=request.user)
+
+    if request.method == "POST":
+        profile_form = UserProfileForm(request.POST, instance=profile)
+        if profile_form.is_valid():
+            profile_form.save()
+            messages.success(request, "Profile updated successfully!")
+        else:
+            messages.error(
+                request, "Update failed. Please insure the form information is valid."
+            )
+    else:
+        # add users data to the profile form
+        profile_form = UserProfileForm(instance=profile)
+
+    # find all previous orders
+    previous_orders = profile.orders.all()
 
     template = "profiles/profile.html"
-    context = {}
+    context = {"profile_form": profile_form, "previous_orders": previous_orders}
 
     return render(request, template, context)
