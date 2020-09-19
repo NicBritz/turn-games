e03762755a7a303db652b4d0e65daa6dfa00eb6d
nico britz <nico.britz@outlook.com>
2020-09-19 16:10:03 +0100

    :sparkles: added email account settings for authentication

--- turn_games/settings.py
+++ turn_games/settings.py
@@ -159,7 +159,7 @@ MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 if 'AWS' in os.environ:
-    # Cache control - for long time
+    # Cache static files for long time
     AWS_S3_OBJECT_PARAMETERS = {
         'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
         'CacheControl': 'max-age=94608000',
@@ -193,3 +193,16 @@ STRIPE_CURRENCY = "gbp"
 
 # email for confirmation
 DEFAULT_FROM_EMAIL = "turngames@example.com"
+
+# Email setup
+if 'DEVELOPMENT' in os.environ:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    DEFAULT_FROM_EMAIL = 'turngames@example.com'
+else:
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_USE_TLS = True
+    EMAIL_PORT = 587
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST')
+    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_USER')
