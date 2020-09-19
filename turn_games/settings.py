32de0001b96dfcf8ebd94c74d34207ee78c75644
nico britz <nico.britz@outlook.com>
2020-09-19 14:39:56 +0100

    :rocket: added aws s3 for static files

--- turn_games/settings.py
+++ turn_games/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     "checkout",
     "dashboard",
     "mathfilters",
+    "storages",
 ]
 
 MIDDLEWARE = [
@@ -156,6 +157,32 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
+if 'AWS' in os.environ:
+    # Cache control - for long time
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
+
+    # Bucket Configuration
+    AWS_STORAGE_BUCKET_NAME = 'turn-games'
+    AWS_S3_REGION_NAME = 'eu-west-1'
+    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+
+    # Static and media files
+    STATICFILES_STORAGE = 'custom_storages.StaticStorage'
+    STATICFILES_LOCATION = 'static'
+    DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
+    MEDIAFILES_LOCATION = 'media'
+
+    # Override static and media URLs in production
+    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
+    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
+
+
 TAX_PERCENTAGE = 0.2
 
 # Stripe
