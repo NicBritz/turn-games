5da5fa71c31e45b48a0c8864ca42468916cde26d
nico britz <nico.britz@outlook.com>
2020-09-24 20:06:12 +0100

    :hammer: removed the calculated price field from game model

--- games/models.py
+++ games/models.py
@@ -70,7 +70,6 @@ class Game(models.Model):
         max_digits=10, decimal_places=0, null=True, blank=True, default=0
     )
     price = models.DecimalField(max_digits=6, decimal_places=2, default=0)
-    price_calculated_value = models.DecimalField(max_digits=6, decimal_places=2, default=0, null=True, blank=True,)
     featured = models.BooleanField(default=False)
     discounted = models.BooleanField(default=False)
     discount_percent = models.DecimalField(
