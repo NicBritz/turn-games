30ce4729470aa65a258920512225ad3cae4f09b7
nico britz <nico.britz@outlook.com>
2020-09-27 17:10:51 +0100

    :rotating_light: implemented tests for home page and validated css html and js

--- home/views.py
+++ home/views.py
@@ -4,33 +4,32 @@ from django.db.models import Q
 
 
 def handler404(request, exception):
+    """ return a custom 404 template """
     return render(request, "404.html", status=404)
 
 
 def handler500(request):
+    """ return a custom 505 template """
     return render(request, "500.html", status=500)
 
 
 def index(request):
     """
-    Renders the games page passing in all the products
+    Renders the home page, with featured and discounted games
     """
 
-    # get all games in the database
+    # get all the games in the database
     games = Game.objects.all()
-    # filter the specials and featured
-    featured_specials = games.filter(Q(discounted=True) | Q(featured=True)).order_by(
-        "?"
-    )
-    # randomly select 10 for main slider
+
+    # filter the specials and featured ordered randomly
     # SRC: https://stackoverflow.com/questions/3506678/
     # in-django-how-do-i-select-100-random-records-from-the-database/3506692
-    random_slider_content = featured_specials[:10]
+    featured_specials = games.filter(Q(discounted=True) | Q(featured=True))
+    random_slider_content = featured_specials.order_by("?")[:10]
 
     context = {
         "featured_specials": featured_specials,
         "random_slider_content": random_slider_content,
     }
-    url = "home/index.html"
 
-    return render(request, url, context)
+    return render(request, "home/index.html", context)
