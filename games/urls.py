23bf2458002e4c7a0a6731e29e7f173fb6876d91
nico britz <nico.britz@outlook.com>
2020-09-27 15:51:24 +0100

    :sparkles: added a basic rating stystem

--- games/urls.py
+++ games/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.all_games, name="games"),
-    path('<game_id>', views.game_detail, name="game_detail"),
+    path('<game_id>/', views.game_detail, name="game_detail"),
+    path('rate_game/<game_id>/<rating>/', views.rate_game, name="rate_game"),
 ]
