aac56399e321c41122da8e3229a36b0c5ed52add
nico britz <nico.britz@outlook.com>
2022-08-21 14:39:47 +0100

    update static files

--- turn_games/settings.py
+++ turn_games/settings.py
@@ -7,7 +7,7 @@ import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get(
